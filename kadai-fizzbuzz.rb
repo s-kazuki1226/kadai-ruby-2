dbb9c3ce1a2f237be4d3bd1d5b7dc5bf04e69688
kazuki.shida <kazuki.shida1226@gmail.com>
2021-10-28 14:00:52 +0000

    kadai-ruby-2

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(num)
+    if num % 15 == 0
+        "FizzBuzz"
+    elsif num % 5 == 0
+        "Buzz"
+    elsif num % 3 == 0
+        "Fizz"
+    else
+        num
+    end    
+end
+
+num_max = 100
+
+(1..num_max).each do |num|
+    puts fizzbuzz(num)
+end    
